b2ff980dda5b1d5f34a4a344f73da3ddc03c5761
bryan <bryanmendez21@icloud.com>
2026-04-13 13:48:29 -0400

    split full name based on spaces then got the number of items in array if items were less than two middle name prints out as none and first name gets first index and last name gets second. if 3 items in a array there will be a first middle and last name.

--- src/main/java/com/pluralsight/full_name_parser.java
+++ src/main/java/com/pluralsight/full_name_parser.java
@@ -6,9 +6,21 @@ public class full_name_parser {
     public static void main(String[] args) {
         Scanner dataCatcher = new Scanner(System.in);
 
-        System.out.println("what is your full name? provide first middle and last name: ");
+        System.out.println("what is your full name? \nprovide first middle and last name \nif no middle name write none: ");
         String fullName = dataCatcher.nextLine();
 
+        String[] nameSplit = fullName.split(" ");
+        int number_of_items = nameSplit.length;
+
+        if(number_of_items < 3) {
+            System.out.println("First name: " + nameSplit[0]);
+            System.out.println("Middle name: (none) ");
+            System.out.println("Last name: " + nameSplit[1]);
+        } else{
+            System.out.println("First name: " + nameSplit[0]);
+            System.out.println("Middle name: " + nameSplit[1]);
+            System.out.println("Last name: " + nameSplit[2]);
+        }
 
     }
 }
