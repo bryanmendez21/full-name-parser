6a44164adb84fdbcc8c831d160923ac150aa27d9
bryan <bryanmendez21@icloud.com>
2026-04-13 13:25:59 -0400

    created scanner to ask for users name

--- src/main/java/com/pluralsight/full_name_parser.java
+++ src/main/java/com/pluralsight/full_name_parser.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class full_name_parser {
+    public static void main(String[] args) {
+        Scanner dataCatcher = new Scanner(System.in);
+
+        System.out.println("what is your full name? provide first middle and last name: ");
+        String fullName = dataCatcher.nextLine();
+
+
+    }
 }
