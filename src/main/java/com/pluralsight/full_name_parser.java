bf91d30855efd5e09d5026c8899546c5b714a8ad
bryan <bryanmendez21@icloud.com>
2026-04-13 13:57:30 -0400

    Added a trim incase user types space's before or after typing up full name.

--- src/main/java/com/pluralsight/full_name_parser.java
+++ src/main/java/com/pluralsight/full_name_parser.java
@@ -9,7 +9,7 @@ public class full_name_parser {
         System.out.println("what is your full name? \nprovide first middle and last name \nif no middle name write none: ");
         String fullName = dataCatcher.nextLine();
 
-        String[] nameSplit = fullName.split(" ");
+        String[] nameSplit = fullName.trim().split(" ");
         int number_of_items = nameSplit.length;
 
         if(number_of_items < 3) {
